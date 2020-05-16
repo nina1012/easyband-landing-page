0e792439905dc2ee1d5b37598cd54f4dbf3aef2d
nina1012 <labferment10@gmail.com>
2020-05-17 00:30:59 +0200

    markup and styling utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      colors: {}
+      screens: {
+        sm: '375px',
+        md: '768px',
+        xl: '1440px'
+      },
+      colors: {
+        // primary
+        'dark-blue': 'hsl(233,26%,24%)',
+        'lime-green': 'hsl(136,65%,51%)',
+        'bright-cyan': 'hsl(192,70%,51%)',
+        //neutral
+
+        'grayish-blue': 'hsl(233,8%,62%)',
+        'light-grayish-blue': 'hsl(230,16%,96%)',
+        'very-light-blue': 'hsl(0,0%,98%)',
+        white: 'hsl(0,0%,100%)'
+      }
     }
   },
   variants: {
